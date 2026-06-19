952a70b124f3a8ec4cc6cea3da2634b812c2fc93
Sanaullah <sanaarman033@gmail.com>
2026-06-19 02:56:59 -0700

    Add the vercel URL

--- server.js
+++ server.js
@@ -15,8 +15,11 @@ const app = express();
 //Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin: [
+      "http://localhost:5173",
+      "https://dastoor-shawls-maker.vercel.app/"
+    ],
+    credentials: true
   })
 );
 
